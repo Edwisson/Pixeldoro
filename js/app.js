7fb8feec662f5b537c4181e8ef18c55423594c0e
Remanuel <edwisson.m.r.l@gmail.com>
2024-11-06 20:00:30 -0400

    boton de pausa, solucion de bugs y muestra de la sesion actual

--- js/app.js
+++ js/app.js
@@ -8,13 +8,14 @@ const task = {
 };
 
 let tasks = [];
-
 let tareaActiva;
+let contadorActivo = false;
 
 const nombreTarea = document.querySelector('#nombre');
 const sesiones = document.querySelector('#sesiones');
 const duracion = document.querySelector('#duracion');
 const descanso = document.querySelector('#descanso');
+const sesion = document.querySelector('.pomodoro__sesion');
 
 formTask.addEventListener('submit', registrarTarea);
 nombreTarea.addEventListener('input', datosFormulario);
@@ -33,11 +34,10 @@ function registrarTarea(e) {
     if (task.nombre === '' || task.sesiones == 0 || task.duracion == 0 || task.descanso == 0) {
         alert("Error: Campos incompletos");
     } else {
-        tasks.push({ ...task });  // Línea cambiada: agregar una copia de `task` a `tasks`
-        crearTarea(tasks.length - 1);  // Línea cambiada: pasar el índice de la tarea a crear
+        tasks.push({ ...task });  
+        crearTarea(tasks.length - 1); 
         console.log('Tarea registrada correctamente');
 
-        // Restablecer los valores de `task` y el formulario
         task.nombre = '';
         task.sesiones = 0;
         task.duracion = 0;
@@ -52,112 +52,123 @@ function registrarTarea(e) {
 function crearTarea(index) {
     if (tasks.length <= 4) {
         const tareasContainer = document.querySelector('.tareas');
-
-        // Crear el contenedor de la sección de la tarea
         const tareasSeccion = document.createElement('div');
         tareasSeccion.classList.add('tareas__seccion');
-
-        // Crear el contenedor de la tarea
         const tareaDiv = document.createElement('div');
         tareaDiv.classList.add('tareas__tarea');
-
-        // Crear el título de la tarea
         const tareaTitulo = document.createElement('h3');
         tareaTitulo.classList.add('no-margin', 'tarea__titulo');
         tareaTitulo.textContent = `${tasks[index].nombre} ${tasks[index].sesiones * tasks[index].duracion + (tasks[index].sesiones - 1) * tasks[index].descanso} min`;
-
-        // Crear el botón de eliminación
         const botonEliminar = document.createElement('button');
         botonEliminar.classList.add('tareas__boton');
         botonEliminar.textContent = 'X';
 
-        // Agregar un evento de clic al botón para eliminar la tarea
         botonEliminar.addEventListener('click', () => {
+            detener();
             tareasSeccion.remove();
-            tasks.splice(index, 1);  // Línea cambiada: eliminar la tarea del array `tasks`
-            console.log(`Tarea eliminada: ${tasks[index] ? tasks[index].nombre : 'desconocida'}`);
-            let tareaActual = document.querySelector('.tarea__actual')
-            tareaActual.textContent = "Selecciona una tarea"
-            let pomodoroTime = document.querySelectorAll('.pomodoro__time')
-            pomodoroTime[0].textContent = '00'
-            pomodoroTime[1].textContent = '00'
+            tasks.splice(index, 1);
+            let tareaActual = document.querySelector('.tarea__actual');
+            tareaActual.textContent = "Selecciona una tarea";
+            let pomodoroTime = document.querySelectorAll('.pomodoro__time');
+            pomodoroTime[0].textContent = '00';
+            pomodoroTime[1].textContent = '00';
         });
 
-        // Agregar el título de la tarea al contenedor de la tarea
         tareaDiv.appendChild(tareaTitulo);
-
-        // Agregar la tarea y el botón de eliminar al contenedor principal
         tareasSeccion.appendChild(tareaDiv);
         tareasSeccion.appendChild(botonEliminar);
-
-        // Finalmente, agregar la tarea al contenedor principal
         tareasContainer.appendChild(tareasSeccion);
     } else {
         alert('Termina todas las tareas actuales - No te sobrecargues');
     }
 }
 
-// Seleccionar el contenedor de las tareas
 const contenedor = document.querySelector('.tareas');
 
-
 contenedor.addEventListener('click', function (e) {
-
     const tareaSeccion = e.target.closest('.tareas__seccion');
-
     if (tareaSeccion) {
-
         const index = Array.from(contenedor.children).indexOf(tareaSeccion);
-        seleccionarTarea(index)
+        seleccionarTarea(index);
     }
 });
 
-let pomodoroTime = document.querySelectorAll('.pomodoro__time')
+let pomodoroTime = document.querySelectorAll('.pomodoro__time');
 
 function seleccionarTarea(index) {
-    let tareas = document.querySelectorAll('.tareas__tarea')
+    let tareas = document.querySelectorAll('.tareas__tarea');
     tareas.forEach(tarea => tarea.classList.remove('tareas__tarea--activa'));
-    tareas[index].classList.add('tareas__tarea--activa')
-    let tareaTitulo = document.querySelector('.tarea__actual')
-    tareaTitulo.textContent = tasks[index].nombre
-
-    pomodoroTime[0].textContent = tasks[index].duracion
-    pomodoroTime[1].textContent = '00'
-    tareaActiva = tasks[index]
-    tareaActiva.index = index
+    tareas[index].classList.add('tareas__tarea--activa');
+    let tareaTitulo = document.querySelector('.tarea__actual');
+    tareaTitulo.textContent = tasks[index].nombre;
+    pomodoroTime[0].textContent = tasks[index].duracion;
+    pomodoroTime[1].textContent = '00';
+    sesion.textContent = 1;  // Muestra la primera sesión
+    tareaActiva = { ...tasks[index], sesionActual: 1 };
+    tareaActiva.index = index;
 }
 
-async function contador() {
-    while (tareaActiva.sesiones != 0) {
-        let min = tareaActiva.duracion; // Declarar `min` fuera del intervalo para persistir su valor
-        let seg = 0; // Iniciar segundos en 0
-        const intervalo = setInterval(() => {
-            console.log("Ejecutando acción en el intervalo");
+let intervalo;
 
-            if (seg === 0) {
-                min -= 1; // Reducir los minutos
-                seg = 59; // Reiniciar los segundos a 59
-            } else {
-                seg -= 1; // Reducir los segundos
+async function contador() {
+    if (contadorActivo === true) {
+        return;
+    }
+    contadorActivo = true;
+
+    while (tareaActiva.sesiones > 0 && contadorActivo) {
+        let min = tareaActiva.duracion;
+        let seg = 0;
+        sesion.textContent = tareaActiva.sesionActual;
+
+        intervalo = setInterval(() => {
+            if (!contadorActivo) {
+                clearInterval(intervalo);
+                return;
             }
 
-            // Actualizar el DOM con los valores actuales de `min` y `seg`
-            pomodoroTime[0].textContent = min < 10 ? `0${min}` : min
-            pomodoroTime[1].textContent = seg < 10 ? `0${seg}` : seg
+            if (seg === 0) {
+                if (min === 0) {
+                    clearInterval(intervalo);
+                    return;
+                }
+                min -= 1;
+                seg = 59;
+            } else {
+                seg -= 1;
+            }
 
+            pomodoroTime[0].textContent = min < 10 ? `0${min}` : min;
+            pomodoroTime[1].textContent = seg < 10 ? `0${seg}` : seg;
         }, 1000);
 
         await new Promise(resolve => setTimeout(resolve, tareaActiva.duracion * 60 * 1000));
         clearInterval(intervalo);
-        tareaActiva.sesiones -= 1// Detener el intervalo si llega a 0 minutos y 0 segundos
+        tareaActiva.sesiones -= 1;
+        tareaActiva.sesionActual += 1;
     }
-    if (tareaActiva.sesiones == 0) {
-        const tareaSecciones = contenedor.querySelectorAll('.tareas__seccion')
-        tareaSecciones[tareaActiva.index].remove()
+
+    if (tareaActiva.sesiones === 0) {
+        const tareaSecciones = contenedor.querySelectorAll('.tareas__seccion');
+        sesion.textContent = 0
+        tareaSecciones[tareaActiva.index].remove();
         tasks.splice(tareaActiva.index, 1);
     }
+
+    contadorActivo = false;
 }
 
-// Corrección en el selector del botón
 let botonIniciar = document.querySelector('#iniciar');
 botonIniciar.addEventListener('click', contador);
+
+let botonDetener = document.querySelector('#detener');
+botonDetener.addEventListener('click', detener);
+
+function detener() {
+    if (contadorActivo === true) {
+        clearInterval(intervalo);
+        pomodoroTime[0].textContent = tareaActiva.duracion < 10 ? `0${tareaActiva.duracion}` : tareaActiva.duracion;
+        pomodoroTime[1].textContent = '00';
+        contadorActivo = false;
+    }
+}
